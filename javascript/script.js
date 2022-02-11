ae44ac8acda4fcddcc93b17b65088bbd52b82f46
Mark Estrada <h.mark.estrada@gmail.com>
2022-02-11 13:52:25 -0600

    Update script.js
    Added comments and formatting.
    
    No intentional functional changes made.

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,36 @@
 // Display today's day and date
 var today = new Date();
-var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 let month = months[today.getMonth()];
-var date = month+' '+today.getDate()+', '+today.getFullYear();
+var date = month + ' ' + today.getDate() + ', ' + today.getFullYear();
 
+// Gets todays time and day for local storage in 12 hour format
 let displayTime = weekday[today.getDay()];
-var time = today.toLocaleString('en-US', { hour: 'numeric', minute:	"numeric", hour12: true });
+var time = today.toLocaleString('en-US', {
+    hour: 'numeric',
+    minute: "numeric",
+    hour12: true
+});
 
-document.getElementById('currentDay').innerHTML = date+" "+displayTime+" "+time;
+// Displays the current day word and month, day and year
+document.getElementById('currentDay').innerHTML = date + " " + displayTime + " " + time;
 
 $(document).ready(function() {
     // saveBtn click listener 
     $(".saveBtn").on("click", function() {
+
         // Get nearby values of the description in JQuery
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
-        // Save text in local storage
+        // Save item and time in local storage
         localStorage.setItem(time, text);
     })
 
     function timeTrack() {
+        
         //get current number of hours.
         var timeNow = moment().hour();
 
@@ -49,16 +57,16 @@ $(document).ready(function() {
     }
 
     // local storage Get Items
-    $("#hour0 .description").val(localStorage.getItem("hour0"));
-    $("#hour1 .description").val(localStorage.getItem("hour1"));
-    $("#hour2 .description").val(localStorage.getItem("hour2"));
-    $("#hour3 .description").val(localStorage.getItem("hour3"));
-    $("#hour4 .description").val(localStorage.getItem("hour4"));
-    $("#hour5 .description").val(localStorage.getItem("hour5"));
-    $("#hour6 .description").val(localStorage.getItem("hour6"));
-    $("#hour7 .description").val(localStorage.getItem("hour7"));
     $("#hour8 .description").val(localStorage.getItem("hour8"));
     $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
     timeTrack();
 })
